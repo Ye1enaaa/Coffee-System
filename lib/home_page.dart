3d213f137d565a8af8a57a8b6cb0e28310c4645b
EErickson <avilasonson@gmail.com>
2023-07-12 21:16:08 +0800

    fetch data on sqflite ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_system/db_helper.dart';
 import 'package:flutter/material.dart';
 
 import 'form_page.dart';
@@ -10,6 +11,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List <Map<String, dynamic>> dataList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchData();
+  }
+
+  Future <void> _fetchData() async {
+    List<Map<String, dynamic>> data = await DatabaseHelper().fetchData();
+    setState(() {
+      dataList = data;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +33,20 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: const Text('Home Page')
       ),
+      body: RefreshIndicator(
+        onRefresh: _fetchData,
+        child: ListView.builder(
+          itemCount: dataList.length,
+          itemBuilder:(context, index){
+            Map<String, dynamic> data = dataList[index];
+            return ListTile(
+              title: Text(data['customer_name']),
+            );
+          }),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
+          _fetchData();
           Navigator.push(context, MaterialPageRoute(builder: (context)=> const FormPage()));
         },
         child: const Icon(Icons.add_rounded),
