c8533e5a8717e731f880f44b4b6f9ffce00d56e3
EErickson <avilasonson@gmail.com>
2023-07-15 20:52:33 +0800

    save data using sqflite

---
+++ lib/db_helper.dart
@@ -0,0 +1,42 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._();
+  static Database? _database;
+
+  DatabaseHelper._();
+
+  factory DatabaseHelper() => _instance;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'customer_details.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE customer (
+            id INTEGER PRIMARY KEY,
+            customer_name TEXT,
+            customer_number TEXT,
+            customer_address TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertData(Map<String, dynamic> data) async {
+    Database db = await database;
+    return await db.insert('customer', data);
+  }
+
+  // Add other data operations like updating, deleting, querying here...
+}
--- lib/form_page.dart
+++ lib/form_page.dart
@@ -1,5 +1,7 @@
+import 'package:coffee_system/db_helper.dart';
 import 'package:flutter/material.dart';
-
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 class FormPage extends StatefulWidget {
   const FormPage({ Key? key }) : super(key: key);
 
@@ -8,11 +10,56 @@ class FormPage extends StatefulWidget {
 }
 
 class _FormPageState extends State<FormPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  void saveData () async {
+    String name = nameController.text;
+    String number = numberController.text;
+    String address = addressController.text;
+
+    Map<String, dynamic> data = {
+      'customer_name' : name,
+      'customer_number' : number,
+      'customer_address' : address
+    };
+    int result  = await DatabaseHelper().insertData(data);
+    if(result != null){
+      print(data);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Customer'),
+      ),
+      body: Form(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.person_sharp),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: numberController,
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: addressController,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: (){
+                saveData();
+              }, child: const Text('SUBMIT'))
+          ],
+        ) 
       ),
     );
   }
